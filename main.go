eb96f1f92cafd4ee9a2ac7fea5a0ebd7983c309a
Dominic Steiner <dominic.st18@gmail.com>
2018-05-21 16:03:59 +0200

    generate random values for second choice and store this array

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
 )
 
 type Calc struct {
@@ -10,8 +14,47 @@ type Calc struct {
 }
 
 func main() {
-	var values = readCSV("./test.csv")
-	fmt.Println(values)
-	fmt.Println("haha")
+
+	fmt.Println("random vlaues -> type 1 \nvalues from csv -> type 2")
+	var input int = 0
+	fmt.Printf(">>")
+	fmt.Scanln(&input)
+	if input == 1 {
+		var randoemValue = generateOutPut()
+		fmt.Println(randoemValue)
+	} else if input == 2 {
+		var values = readCSV("./test.csv")
+		fmt.Println(values)
+	} else {
+		fmt.Println("okay, bey!")
+		os.Exit(3)
+	}
+
+}
+
+func random(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min) + min
+}
+
+func generateOutPut() []Calc {
+	var value1 = 0
+	var value2 = 0
+	var values []Calc
+	for i := 0; i <= 20; i++ {
+		value1 = random(1, 10)
+		value2 = random(1, 10)
+		if i%2 == 0 && i != 0 {
+			show := strconv.Itoa(value1) + "+" + strconv.Itoa(value2)
+			data := Calc{
+				Res:  value1 + value2,
+				Show: show,
+			}
+			values = append(values, data)
+
+		}
+
+	}
+	return values
 
 }
